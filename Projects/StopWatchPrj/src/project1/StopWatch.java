d76cfc35e041d38e9b5064f8bf3020356b409dca
David Whynot <davidmwhynot@gmail.com>
2018-05-09 19:47:34 -0400

    added lab 1 files

---
+++ Projects/StopWatchPrj/src/project1/StopWatch.java
@@ -0,0 +1,55 @@
+package project1;
+
+public class StopWatch {
+	private int minutes;
+	private int seconds;
+	private int milliseconds;
+	
+	public String toString() {
+		return "StopWatch [minutes=" + minutes + ", seconds" + seconds + ", milliseconds=" + milliseconds + "]";
+	}
+	
+	public StopWatch() {
+		super();
+	}
+	public StopWatch(String startTime) {
+		 System.out.println("todo");
+	}
+	public StopWatch(int milliseconds) {
+		super();
+		this.milliseconds = milliseconds;
+	}
+	public StopWatch(int seconds, int milliseconds) {
+		super();
+		this.seconds = seconds;
+		this.milliseconds = milliseconds;
+	}
+	public StopWatch(int minutes, int seconds, int milliseconds) {
+		super();
+		this.minutes = minutes;
+		this.seconds = seconds;
+		this.milliseconds = milliseconds;
+	}
+
+
+	public int getMinutes() {
+		return minutes;
+	}
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
+	public int getSeconds() {
+		return seconds;
+	}
+	public void setSeconds(int seconds) {
+		this.seconds = seconds;
+	}
+	public int getMilliseconds() {
+		return milliseconds;
+	}
+	public void setMilliseconds(int milliseconds) {
+		this.milliseconds = milliseconds;
+	}
+	
+	
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# CIS 163
+# GVSU
+### David Whynot
+readme TODO
